02bccb405a0e058fc063059689774ed18b4354ce
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2022-07-18 13:14:54 +0530

    xgxcv

---
+++ testingspock/src/main/java/com/testingspock/Add.java
@@ -0,0 +1,7 @@
+package com.testingspock;
+
+public class Add {
+	public int addTwoNumber(int i,int j) {
+		return i+j;
+	}
+}
---
+++ testingspock/src/test/java/com/testingspock/AddSpec.groovy
@@ -0,0 +1,14 @@
+package com.testingspock
+
+
+import spock.lang.*
+
+class AddSpec extends Specification{
+	
+	def "Add Two Number"(){
+		when: "Initializing a class"
+		def add=new Add();
+		then: "result of numbers "
+		add.addTwoNumber(3,5) == 8
+	}
+}
